789b53c49ad739fe64367914b9ee7e4d8892602b
basker-bsk <basker.sundaramoorthy@borngroup.com>
2020-06-25 11:38:11 +0530

    Updates

---
+++ precache-manifest.90d5212212a9f390a77234dc93b6eb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee590aa9b511af2d943a876160baa28c",
+    "url": "/React-API/index.html"
+  },
+  {
+    "revision": "b4c2d5cae9b79f6b1968",
+    "url": "/React-API/static/css/main.c6380ec3.chunk.css"
+  },
+  {
+    "revision": "8b9139dc3502cb11a909",
+    "url": "/React-API/static/js/2.3184f4e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-API/static/js/2.3184f4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c2d5cae9b79f6b1968",
+    "url": "/React-API/static/js/main.3a84cb7c.chunk.js"
+  },
+  {
+    "revision": "ac677913fcd4231b8358",
+    "url": "/React-API/static/js/runtime-main.c4bf1478.js"
+  },
+  {
+    "revision": "79136081229982841d2f6ad212f593e9",
+    "url": "/React-API/static/media/logo.79136081.png"
+  }
+]);
